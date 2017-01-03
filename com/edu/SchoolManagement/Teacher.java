86efff8fa488dddfef3962edf163cf64f4aab757
Vibhav Kanyadan <kvibhav6@gmail.com>
2017-01-03 13:05:27 -0500

    Add to Teacher class functionality.

--- com/edu/SchoolManagement/Teacher.java
+++ com/edu/SchoolManagement/Teacher.java
@@ -4,4 +4,9 @@ package com.edu.SchoolManagement;
  * Created by vibs on 1/1/17.
  */
 public class Teacher {
+    private final String name_;
+
+    public Teacher(String name_) {
+        this.name_ = name_;
+    }
 }
