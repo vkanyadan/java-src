3146504ff1e274437bd200fefa89a8d4106c8823
Vibhav Kanyadan <kvibhav6@gmail.com>
2017-01-05 20:04:32 -0500

    Add to Restaurant Management.

---
+++ com/edu/ResturantManagement/Restaurant.java
@@ -0,0 +1,18 @@
+package com.edu.ResturantManagement;
+
+/**
+ * Created by vibs on 1/5/17.
+ */
+public class Restaurant {
+    private final String name_;
+    private final String cuisine_;
+
+    public Restaurant(String name_) {
+        this.name_= name_;
+        this.cuisine_ = cuisine_;
+
+
+
+
+    }
+}
---
+++ com/edu/ResturantManagement/RestaurantManagement.java
@@ -0,0 +1,10 @@
+package com.edu.ResturantManagement;
+
+/**
+ * Created by vibs on 1/5/17.
+ */
+public class RestaurantManagement {
+    public static void main(String[] args) {
+
+    }
+}
