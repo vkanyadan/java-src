1c1dc0edde0efeb946039cfe277c916a19dcff56
Balaji Iyengar <briyenga@gmail.com>
2017-01-08 08:49:17 -0800

    Exercise-1

---
+++ com/edu/CodingProblems/Fibonacci.java
@@ -0,0 +1,13 @@
+package com.edu.CodingProblems;
+
+/**
+ * Created by biyengar on 1/8/17.
+ */
+
+public class Fibonacci {
+    static private final long max_term = 1000;
+
+    public static void main(String[] args) {
+        System.out.println("Fib series:");
+    }
+}
